1d11458f8b3ccecb13d424cd67aada445cf5f98c
Nishanth P <nish4anth99@gmail.com>
2023-10-20 22:05:33 -0700

    solved concurrency issue

--- integral.h
+++ integral.h
@@ -1,14 +1,21 @@
 #include "parallel.h"
+#include <atomic>
 
 using namespace parlay;
 
 template <class Func>
 double integral(const Func& f, size_t n, double low, double high) {
-  double ans = 0;
+  // double ans = 0;
+  std::atomic<double> ans = 0;
   parallel_for(0, n, [&](size_t i) {
     double x = low + (high - low) * i / n;
     double dx = (high - low) / n;
-    ans += f(x) * dx;
+    // ans += f(x) * dx;
+    double fxdx = f(x) * dx;
+    double temp_ans = ans;
+    while(!ans.compare_exchange_weak(temp_ans, temp_ans + fxdx)) {
+      temp_ans = ans;
+    }
   });
   // for (size_t i = 0; i < n; i++) {
   //   double x = low + (high - low) * i / n;
