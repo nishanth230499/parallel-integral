00776302461f2fc0a2ac35ce4d71afd1def2494d
Nishanth P <nish4anth99@gmail.com>
2023-10-20 19:43:57 -0700

    changing for to parallel_for

--- integral.h
+++ integral.h
@@ -5,10 +5,15 @@ using namespace parlay;
 template <class Func>
 double integral(const Func& f, size_t n, double low, double high) {
   double ans = 0;
-  for (size_t i = 0; i < n; i++) {
+  parallel_for(0, n, [&](size_t i) {
     double x = low + (high - low) * i / n;
     double dx = (high - low) / n;
     ans += f(x) * dx;
-  }
+  });
+  // for (size_t i = 0; i < n; i++) {
+  //   double x = low + (high - low) * i / n;
+  //   double dx = (high - low) / n;
+  //   ans += f(x) * dx;
+  // }
   return ans;
 }
