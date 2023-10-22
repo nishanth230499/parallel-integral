e92b2d74d050e51731cec0e890ef34ad0b044c34
Nishanth P <nish4anth99@gmail.com>
2023-10-21 22:39:43 -0700

    changing treshold

--- integral.h
+++ integral.h
@@ -6,7 +6,7 @@ using namespace parlay;
 template <class Func>
 double integral(const Func& f, size_t n, double low, double high) {
   double dx = (high - low) / n;
-  if (n < 100000) {
+  if (n < 10000) {
     double seq_sum = 0;
     for(size_t i = 0; i < n; i++) {
       double x = low + dx * i;
