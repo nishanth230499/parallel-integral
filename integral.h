c13c3d6bfa6a2794665ad4636a9d9f89bc057520
Nishanth P <nish4anth99@gmail.com>
2023-10-21 22:23:52 -0700

    using granularity

--- integral.h
+++ integral.h
@@ -5,18 +5,22 @@ using namespace parlay;
 
 template <class Func>
 double integral(const Func& f, size_t n, double low, double high) {
-  // double ans = 0;
-  std::atomic<double> ans = 0;
-  parallel_for(0, n, [&](size_t i) {
-    double x = low + (high - low) * i / n;
-    double dx = (high - low) / n;
-    // ans += f(x) * dx;
-    double fxdx = f(x) * dx;
-    double temp_ans = ans;
-    while(!ans.compare_exchange_weak(temp_ans, temp_ans + fxdx)) {
-      temp_ans = ans;
+  double ans = 0;
+  // std::atomic<double> ans = 0;
+  size_t p = 100000;
+  double temp_sum[p];
+  parallel_for(0, p, [&](size_t i) {
+    temp_sum[i] = 0;
+    for(size_t j = i * n / p; j < (i+1) * n / p; j++) {
+      double x = low + (high - low) * j / n;
+      double dx = (high - low) / n;
+      double fxdx = f(x) * dx;
+      temp_sum[i] += fxdx;
     }
   });
+  for(size_t i=0; i<p; i++) {
+    ans += temp_sum[i];
+  }
   // for (size_t i = 0; i < n; i++) {
   //   double x = low + (high - low) * i / n;
   //   double dx = (high - low) / n;
@@ -24,3 +28,28 @@ double integral(const Func& f, size_t n, double low, double high) {
   // }
   return ans;
 }
+
+
+// template <class Func>
+// double integral(const Func& f, size_t n, double low, double high) {
+//   // printf("%ld\n", n);
+//   double dx = (high - low) / n;
+//   if (n < 100000) {
+//     double seq_sum = 0;
+//     for(size_t i = 0; i < n; i++) {
+//       double x = low + dx * i;
+//       double fxdx = f(x) * dx;
+//       seq_sum += fxdx;
+//     }
+//     return seq_sum;
+//   } else {
+//     double v1, v2;
+//     size_t halfn = n / 2;
+//     double midx = low + dx * halfn;
+//     // printf("%f, %f, %ld\n", low, high, n);
+//     auto f1 = [&]() { v1 = integral(f, halfn, low, midx); };
+//     auto f2 = [&]() { v2 = integral(f, n - halfn, midx, high); };
+//     par_do(f1, f2);
+//     return v1 + v2;
+//   }
+// }
